897f4597de302ee6a2761152fdb47430356a489f
wqq514 <wqq514@naver.com>
2022-08-19 17:21:12 +0900

    emcc finish
    emcc test finish

--- src/main/webapp/script/main.js
+++ src/main/webapp/script/main.js
@@ -1,11 +1,2 @@
-const burger = document.querySelector("#opener");
-	const layer = document.querySelector("#layer-popup");
 
-	burger.addEventListener("click",function(){
-		layer.classList.add("show");
-	})
-
-	const closeBtn = document.querySelector("#closer");
-	closeBtn.addEventListener("click",function(){
-		layer.classList.remove("show");
-	})
+		
